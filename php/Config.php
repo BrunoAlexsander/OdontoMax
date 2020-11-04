34aea62ef90effbdcffb38f656a3fd1f091e9ea0
Elieljrr <elieljrr@gmail.com>
2020-11-04 19:53:35 -0200

    Implementação de Configuração e conexão

---
+++ php/Config.php
@@ -0,0 +1,9 @@
+<?php
+// Website configuration
+if (!defined('DB_HOST')) define("SITE_NAME", "OdontoMax");
+
+// Database configuration
+if (!defined('DB_HOST')) define("DB_HOST", "localhost");
+if (!defined('DB_NAME')) define("DB_NAME", "odontomax");
+if (!defined('DB_USER')) define("DB_USER", "root");
+if (!defined('DB_PASS')) define("DB_PASS", "vertrigo");
